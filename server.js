3adbd45d39042f5b9d6a834e8f4d944fbfc9a767
jayesh-jain-11 <jayeshrlhj.r@gmail.com>
2025-12-29 15:22:57 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = 3000;
 
+const counter = (() => {
+  let count = 0;
+  return () => ++count;
+})();
+
 const server = http.createServer((req, res) => {
-    res.end("Server is up and running!");
+  res.end(`Server is up and running!, ${counter()} requests served.`);
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
